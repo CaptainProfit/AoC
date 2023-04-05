1fa32fd561f6aa5cb0e2dee995af7e0937a241f6
Kesha <akilae@ya.ru>
2023-04-05 07:57:52 +0300

    играл в операторы/конструкторы/классы. И проиграл.
    по ночному городу - в обертке катал

--- 2022/day14/day14_1.cpp
+++ 2022/day14/day14_1.cpp
@@ -8,94 +8,165 @@
 #include <string>
 #include <iterator>
 #define ull unsigned long long
-string conf = "test";
+#define CENTERX 500
 using namespace std;
+string conf = "test";
 
-class cPath{
-	vector<cPoint> a;
-	cPoint* it = nullptr;
-	cPoint dir;
-	int it = 0;
+typedef enum{eleft = 0, eright = 1, eup = 2, edown = 3} eDir;
+class cPoint{
+	int b;
+	int c;
 	public:
-	void addPoint(cPoint &b){
-		a.emplace_back(b);
-	}
-	void addPoint(const string& b){
-		a.emplace_back(b);
-	}
-	void offSet(int ofs){
-		for(vector<cPoint>::iterator b = a.begin(); b != a.end(); b++){
-			b->x -= ofs;
-		}
+	int z;
+	int x;
+	int y;
+	
+	cPoint(const int &nx, const int &ny){
+		x = nx;
+		y = ny;
 	}
 	
-	void initIt(){
-		if(a.size() == 0)
-			return;
-		it = &a[0];
-		it = 0;
-		if(a.size() == 1)
-			return;		
-		dir = a[0]>>a[1];
-	}
-	cPoint it(){		
-		if(it != a[it+1]){
-			it +=dir;
-			return it;
-		}
-		else{
-			it++;
-			if(it != a.size() - 1){
-				dir = a[it]>>a[it + 1];
-			}
-		}
-	}
-};
-
-class cPoint{
-	static int errors;
-	public:
-	int x, y;
-	cPoint(int nx, int ny){
-		x=nx;
-		y=ny;
-	}
 	cPoint(const string& s){
 		int d = s.find(',');
-		if(d == -1)
-			errors++;
 		string sx = s.substr(0, d);
 		string sy = s.substr(d+1, s.length());
 		x = stoi(s.substr(0, d));
 		y = stoi(s.substr(d+1, s.length()));
 	}
-	void offset(int ofs){
-		x -= ofs;
+
+	void operator>>(const eDir& dir);
+
+	eDir operator-(cPoint& start){
+		if(start.x > this->x){
+			return eleft;
+		} 
+		if(start.x < this->x){
+			return eright;
+		}
+		if(start.y > this->y){
+			return eup;
+		} 
+		return edown;
 	}
 
-	friend cPoint& operator>>(cPoint& src, cPoint& dst){
-		if(src.x == dst.x){
-			if(src.y > dst.y){
-				return dir[up];
-			}
-			else{
-				return dir[down];
-			}
-		}
-		else{
-			if(src.x > dst.x){
-				return dir[left];
-			}
-			else{
-				return dir[right];
-			}
-		}
+};
+
+bool cPoint::operator!=(cPoint& a, cPoint& b){
+		return (a.x != b.x) || (a.y != b.y);
+	}
+
+cPoint dirs[4]={{-1,0},{1,0},{0,-1},{0,1}};
+
+cPoint& cPoint::operator+=(eDir dir){
+	this->x += dirs[dir].x;
+	this->y += dirs[dir].y;
+	return *this;
+}
+
+void cPoint::operator>>(const eDir& dir){
+	this->x+= dirs[dir].x;
+	this->y+= dirs[dir].y;
+}
+
+template<class T>
+class c2vec{
+	int ofs;
+	vector<T> leftv;
+	T center;
+	vector<T> rightv;
+	int min = CENTERX;
+	int max = CENTERX;
+	public:
+	void setOfs(int i){
+		ofs = i;
+	}
+
+	T& operator[](int i){
+		if(i > CENTERX)
+			return rightv[i - 1];
+		if(i < CENTERX)
+			return leftv[i - 1];
+		return center;		
+	}
+	
+	c2vec(int min, int max, T c){
+		leftv.resize(min, c);
+		center = c;
+		rightv.resize(max, c);
+	}
+
+	ostream& operator<<(ostream& os){
+		for(int i = -leftv.size() ; i < rightv.size() ; i++)
+			os<<this[i];
+		return os;
+	}
+
+	void resize(int minx, int maxx, T def){
+		center = def;
+		leftv.resize(CENTERX - minx, def);
+		rightv.resize(maxx - CENTERX, def);
 	}
 };
-enum{right, left, down, up};
-cPoint src(500, 0), dir[4]={{1,0},{-1,0},{0,1},{0,-1}};
 
-vector<vector<char>> map;
+cPoint src(500, 0);
+
+class cMap{
+	vector<c2vec<char>> map;
+	bool isPen;
+	char pen;
+	cPoint penPos;
+	int minx;
+	int maxx;
+	public:
+	
+	void startPen(cPoint&n,  const char& symb){
+		isPen = true;
+		penPos = n;		
+		pen = symb;
+		map[penPos.y][penPos.x] = pen;
+	}
+
+	void continuePen(cPoint n ){
+		eDir dir = n - penPos;
+		while(penPos != n){
+			penPos += dir;
+			map[penPos.y][penPos.x] = pen;
+		}
+		map[penPos.y][penPos.x] = pen;
+	}
+
+	void endPen(){
+		isPen = false;
+	}
+
+	void convex(cPoint& n){
+		bool resize = false;
+		if(map.size()>= n.y){
+			map.resize(n.y + 1);
+			resize = true;
+		}
+		if(minx < n.x){
+			minx = n.x;
+			resize = true;
+		}
+		if(maxx < n.x){
+			maxx = n.x;
+			resize = true;
+		}
+		for(int i = 0; i < map.size(); i ++){
+			map[i].resize(minx, maxx, '.');
+		}
+	}
+
+	c2vec<char>& operator[](int p){
+		return map[p];
+	}
+
+	int size(){
+		return map.size();
+	}
+} map;
+
 int readFileToSMt(){	
 	string line;
 	ifstream ifstr(conf+".input", ios::binary);
@@ -105,49 +176,31 @@ int readFileToSMt(){
 	int maxx = 500;
 	while(1){
 		getline(ifstr, line);
-
-		paths.emplace_back();
-		int i = paths.size()-1;		
 		int caret = 0;
-		while(caret < line.length() &&
-				caret != -1){
-			int interval = line.find('-') - caret;
-			cPoint p(line.substr(caret, interval - 1));
+		int interval = line.find('-') - caret;
+		cPoint a(line.substr(caret, interval - 1));
+		map.startPen(a, '#');
+		map[a.y][a.x] = 's';
+
+		while(1){			
 			caret = interval + 2;//skip ->
-			if(p.y>maxy){
-				maxy = p.y;
+			interval = line.find('-', caret) - caret;
+			if(caret >= line.length() ||
+				caret == -1){
+					break;
 			}
-			if(p.x>maxx){
-				maxx = p.x;
-			}
-			if(p.x<minx){
-				minx = p.x;
-			}
-			paths[i].addPoint(p);
+			cPoint b(line.substr(caret, interval - 1));
+			map[b.y][b.x] = 'e';
+			map.continuePen(b);			
+			a = b;
 		}
-		
+		map.endPen();
 		if(ifstr.eof()){
 			ifstr.close();
 			break;
 		}
 	}
 	//postprocessings:
-	map.resize(maxy+1);
-	int sizex = maxx - minx + 1;
-	for(line:map)
-		line.resize(sizex, ',');
-	
-	int ofs = minx;
-	src.offset(ofs);
-	for path:paths;
-		path.offset(ofs);
-	
-	map[src] = '*';
-	for(path:paths){
-		for(it:path){
-			map[it] = '#';
-		}
-	}
 	return 0;
 }
 
@@ -159,10 +212,7 @@ void writeFileSmt(){
 	int result = 0;
 	ofstream ofstr(conf+".output", ios::binary);
 	for(int i = 0 ; i < map.size(); i++){
-		for(int j = 0; j < map[i].size(); j++){
-			ofstr<<map[i][j];
-		}
-		ofstr<<endl;
+		ofstr<<map[i]<<endl;
 	}
 	ofstr<<"there are "<<result<<" in result"<<endl;
 	ofstr.close();
