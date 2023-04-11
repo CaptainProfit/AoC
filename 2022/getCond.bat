6449759114a31db82f8a98f23d76447f5657cbd9
Kesha <akilae@ya.ru>
2023-04-11 07:37:54 +0300

    вытащил сессию из скрипта

--- 2022/getCond.bat
+++ 2022/getCond.bat
@@ -1,9 +1,11 @@
-::[UTF-8 en/ru]
-::using format get 2022 5
-::my_cookie - session id надо вручную подставлять! пока сессия не отвалится.
-::хром, отладка, network, тыкаю get your puzzle input, появляется пакет input, парсю глазками, нахожу куки, в них нахожу session 
-echo off :: не пали мои сессии!
-set my_cookie="session=5xxx5;"
+:: [UTF-8 en/ru]
+:: using format get 2022 5
+:: my_cookie - session id надо вручную подставлять! пока сессия не отвалится.
+:: хром, отладка, network, тыкаю get your puzzle input, 
+:: появляется пакет input, парсю глазками, нахожу куки, в них нахожу session=огромное слово
+:: результат положить в session.cookie - сам файл в гит игноре.
+echo off
+if exist session.cookie (set /p my_cookie=<session.cookie) else (echo configure session in first)
 set YEAR=2022
 set DAY=%1
 set OUT_DIR=day%DAY%
@@ -11,4 +13,11 @@ if not exist %OUT_DIR% mkdir %OUT_DIR%
 set urlPath="https://adventofcode.com/%YEAR%/day/%DAY%/input"
 set FILE_OUT="%OUT_DIR%/cond.input"
 curl --cookie "%my_cookie%" "%urlPath%" > %FILE_OUT%
-copy template.cpp %OUT_DIR%/day%DAY%_1.cpp
+copy template.cpp "%OUT_DIR%/day%DAY%_1.cpp"
+cd %OUT_DIR%
+start /b code . "day%DAY%_1.cpp"
+pause 5
+:: почему не работает выход?
+
+exit
+pause 1
