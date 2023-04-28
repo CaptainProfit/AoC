43975b3a1ecea1116fd6cb74f02e5e4715df5a02
i.romanchenko <i.romanchenko@elesta.ru>
2023-04-28 14:48:06 +0300

    продвинулся, хапнул проблемы с кодировками, начал рисовать несколько состояний подряд.

--- 2022/day17/day17_1.cpp
+++ 2022/day17/day17_1.cpp
@@ -1,14 +1,20 @@
 //[UTF-8 en/ru]
 //problem - https://adventofcode.com/2022/day/x
+// https://en.cppreference.com/w/cpp/string/multibyte/mbstowcs
+// https://en.cppreference.com/w/cpp/string/basic_string/size
 //2147483647 - LONG_MAX
 //18446744073709551615 - ULLONG_MAX
 #include <iostream> 
 #include <fstream>
 #include <vector>
 #include <string>
+#include <array>
 #define ull unsigned long long
-
+#define screenHeight 20
+#define screenWidth 20
 using namespace std;
+
+using my_string=u32string;
 
 //'-' '+' 'L' '|' '#' ;
 //█ - 219
@@ -31,11 +37,12 @@ class cFigures{
 		sprites[0].push_back({0, 3});
 		
 		//  █ 
-		// █ █
+		// ███
 		//  █ 
 		heights[1] = 3;
 		sprites[1].push_back({0, 1});
 		sprites[1].push_back({1, 0});
+		sprites[1].push_back({1, 1});
 		sprites[1].push_back({1, 2});
 		sprites[1].push_back({2, 1});		
 		
@@ -87,7 +94,7 @@ class cSolve{
 	vector<char> directions;
 	int dirIt = 0;
 	
-	const int restCounterFinish = 5; //2023	
+	const int restCounterFinish = 15; //2023	
 	
 	int height = 0;
 	vector<vector<char>> map;
@@ -96,7 +103,7 @@ class cSolve{
 	
 	public:
 	//1 init from file
-	cSolve():cSolve("test.input"){}
+	//cSolve():cSolve(){}
 	cSolve(string filename){	
 		string line;
 		ifstream ifstr(filename, ios::binary);
@@ -188,42 +195,95 @@ class cSolve{
 				}
 				dirIt++;
 				dirIt %= directions.size();
-				print();
+				// print();
 			}while(moveTo({-1, 0}));
 			paintSpriteAtPosAs('#');
+			print();
 			recalcHeight();
 		}		
-		print();
-		currentSprite = nullptr;
+		print(true);
+		currentSprite = nullptr;		
 		return height;
 	}
 
 	//выводит состояние колодца в данный момент времени.
-	void print(){
+	array<string, screenHeight> screen;
+	void print(bool isFlush = false){
 		static int j = 0;
-		//system("cls");
-		cout<<"step "<<j++<<endl;
-		int i;
-		for(i = map.size() - 1; (i >= 0) && (i>= height - 5); i--){
-			cout<<"║"<<string(map[i].data())<<"║";
-			if( i%10 == 9){
-				cout<<i+1<<endl;
+		static int k = 0;
+		static const int kMax = (100/screenWidth); // states per line
+		// system("cls");
+		if(!isFlush){
+			screen[0] += "step " + to_string(j);
+			
+			if(height < screenHeight - 1){
+				screen[screenHeight - 1] += "|-------|"; //╚ ═╝║
+			}
+			else
+				screen[screenHeight - 1] += "|~~~~~~~|";
+			int iter = map.size() - 1;
+			if(iter < screenHeight - 2){
+				iter = screenHeight - 2;
 			}
 			else{
-				cout<<endl;
+				iter += 2;
+			}
+			for(int i = 1; i < screenHeight - 1; i++){
+				string data(7, ' ');
+				if(iter < map.size()){
+					data = string(map[iter].data());
+				}
+				screen[i] +="|" + data + "|";
+				//метка высоты
+				if( i%10 == 9){
+					screen[i] += to_string(i + 1);
+				}
+				//этот указатель опускаем глубже
+				iter--;
 			}
 		}
-		if(i>= height - 5){
-			cout<<"╚═══════╝"<<endl; 
+		else{
+			// заставить вывести остаток.
+			if(k != 0){
+				k = kMax;
+			}
 		}
-		else
-		cout<<"║~~~~~~~║"<<i+1<<endl; 
+		k++;
+		if(k >= kMax){
+			for(int i = 0; i < screenHeight; i++){
+				cout<<screen[i]<<endl;
+				screen[i] = "";
+			}
+			k = 0;
+		}
+		else{
+			for(int i = 0; i < screenHeight; i++){
+				string additional((k*20 - screen[i].size()), ' ');
+				// for(int j = 0; j < screen[i].size(); j++){
+				// 	cout<<static_cast<unsigned int>(static_cast<unsigned char>(screen[i][j]))<<" ";
+				// }
+				// cout<<endl;
+				screen[i] += additional;
+			}
+		}
 	}
 };
 
 int main(void){
-	cSolve solve;
-	int result = solve.solve();
+	int result;
+
+	cSolve test("test.input");
+	result = test.solve();
+	if(result != 3068){
+		cout<<"test.input failed with "<<result<<" in result(must be 3068)"<<endl;	
+		return -1;
+	}
+	cout<<"test.input passed: "<<result<<"(3068)"<<endl;	
+	return -2;
+
+	cSolve cond("cond.input");
+	result = cond.solve();
+
 	cout<<"there are "<<result<<" in result"<<endl;
 	return 0;
 }
