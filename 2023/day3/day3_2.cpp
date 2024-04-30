8fc8e4d87263057aeb29344862f60a41800cb56f
Innokentiy Romanchenko <akilae@yandex.ru>
2024-04-30 17:27:14 +0300

    день 3, часть 2, почти

--- 2023/day3/day3_2.cpp
+++ 2023/day3/day3_2.cpp
@@ -22,6 +22,7 @@ using namespace chrono;
 
 const set<char> not_interesting_symbols {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '.', '\n', '\r'};
 string digits_str = "01234567890"s;
+
 class cSolve{
 	vector<string> lines;
 	vector<int> results;
@@ -29,19 +30,6 @@ class cSolve{
 	int width, height;
 	string name;
 	high_resolution_clock::duration work_time;
-
-	bool checkSymbol (int i, int j_begin, int j_last) {
-		if (i < 0 || i >= height) {
-			return false;
-		}
-		for (int j = max(j_begin, 0); j <= j_last && j < width; j++) {
-			if (not_interesting_symbols.find(lines[i][j]) ==
-				not_interesting_symbols.end()) {
-				return true;
-			}
-		}
-		return false;
-	}
 
 	pair<int, int> findNumber(const string& line, int offset = 0) {
 		int j_begin = line.find_first_of(digits_str, offset);
@@ -52,21 +40,46 @@ class cSolve{
 		return {j_begin,j_end};
 	}
 
+	vector<int> checkArea (int i, int j_left, int j_right) {
+		vector<int> result;
+		if (i < 0 || i >= height) {
+			return {};
+		}
+		const string& line = lines[i];
+		int offset = max(0, j_left);
+		for(; offset > 0 && '0' <= line[offset] && line[offset] <= '9'; offset--);
+		
+		for(auto [j_begin, j_end] = findNumber(line, offset);
+				j_begin != -1 && j_begin < j_end && j_begin <= j_right && j_end-1 >= j_left;
+				offset = j_end, tie(j_begin, j_end) = findNumber(line, offset)) {
+			int number;
+			from_chars_result conversion_report = from_chars(&line[j_begin], &line[j_end], number);
+			if (conversion_report.ec == std::errc{}) {
+				result.push_back(number);
+			}
+		}
+		return result;
+	}
+
 	int solveLine(int i) {
 		string& line = lines[i];
 		int n = line.length();
-		int offset = 0;
 		int result = 0;
-		for(auto [j_begin, j_end] = findNumber(line, offset); j_begin != -1 && j_begin < j_end; tie(j_begin, j_end) = findNumber(line, offset)) {
-			int number;
-			std::from_chars(&line[j_begin], &line[j_end], number);
-			if (checkSymbol(i, j_begin - 1, j_begin - 1) ||
-			checkSymbol(i, j_end, j_end) ||
-			checkSymbol(i - 1, j_begin - 1, j_end) ||
-			checkSymbol(i + 1, j_begin - 1, j_end)) {
-				result += number;
+		for(int offset = 0, j_star = line.find("*"s, offset); j_star != -1; offset = j_star + 1, j_star = line.find("*"s, offset)) {
+			
+			vector<int> numbers_around;
+			for (int number: checkArea(i, j_star - 1, j_star - 1))
+				numbers_around.emplace_back(number);
+			for (int number: checkArea(i, j_star + 1, j_star - 1))
+				numbers_around.emplace_back(number);
+			for (int number: checkArea(i - 1, j_star - 1, j_star + 1))
+				numbers_around.emplace_back(number);
+			for (int number: checkArea(i + 1, j_star - 1, j_star + 1))
+				numbers_around.emplace_back(number);
+
+			if (numbers_around.size() == 2) {
+			 	result += numbers_around[0] * numbers_around[1];
 			}
-			offset = j_end;
 		}
 		return result;
 	}
@@ -124,9 +137,9 @@ class cSolve{
 
 int main(void) {
 	cSolve example("example.input");
-	cSolve cond("cond.input"); 
-	// 518930 - too low
-	// 521515 - correct
+	cSolve cond("cond.input");
+	// 58910367 - too low
+	// 62339684 - too low
 	example.solve();
 	cond.solve();
 	cout << endl;
