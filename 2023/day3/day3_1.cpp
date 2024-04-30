c3cae0f0a75aab481b3f253bed6f44902ed4d086
Innokentiy Romanchenko <akilae@yandex.ru>
2024-04-30 14:13:25 +0300

    решение первой части, но неверное. почему?

--- 2023/day3/day3_1.cpp
+++ 2023/day3/day3_1.cpp
@@ -8,6 +8,7 @@
 #include <iostream> 
 #include <numeric>
 #include <map>
+#include <set>
 #include <string>
 #include <string_view>
 #include <vector>
@@ -19,18 +20,51 @@
 using namespace std;
 using namespace chrono;
 
+const set<char> not_interesting_symbols {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '.', '\n', '\r'};
+string digits_str = "01234567890"s;
 class cSolve{
 	vector<string> lines;
 	vector<int> results;
 	int total_result;
+	int width, height;
 	string name;
 	high_resolution_clock::duration work_time;
 
-	int solveLine(const string& line) {
-		string_view sw1(line);
+	bool checkSymbol (int i, int j_begin, int j_last) {
+		if (i < 0 || i >= height) {
+			return false;
+		}
+		for (int j = max(j_begin, 0); j <= j_last && j < width; j++) {
+			if (not_interesting_symbols.find(lines[i][j]) ==
+				not_interesting_symbols.end()) {
+				return true;
+			}
+		}
+		return false;
+	}
+
+	pair<int, int> findNumber(const string& line, int offset = 0) {
+		int j_begin = line.find_first_of(digits_str, offset);
+		int j_end = line.find_first_not_of(digits_str, j_begin);
+		return {j_begin, j_end};
+	}
+
+	int solveLine(int i) {
+		string& line = lines[i];
 		int n = line.length();
-		int result;
-		// ...
+		int offset = 0;
+		int result = 0;
+		for(auto [j_begin, j_end] = findNumber(line, offset); j_begin != -1 && j_begin < j_end; tie(j_begin, j_end) = findNumber(line, offset)) {
+			int number;
+			std::from_chars(&line[j_begin], &line[j_end], number);
+			if (checkSymbol(i, j_begin - 1, j_begin - 1) ||
+			checkSymbol(i, j_end, j_end) ||
+			checkSymbol(i - 1, j_begin - 1, j_end) ||
+			checkSymbol(i + 1, j_begin - 1, j_end)) {
+				result += number;
+			}
+			offset = j_end;
+		}
 		return result;
 	}
 
@@ -44,6 +78,14 @@ class cSolve{
 				lines.push_back(line);
 			}
 			ifstr.close();
+			height = lines.size();
+				if (height > 0) {
+				width = lines[0].length();
+			}
+			else {
+				cout << "wrong input" << endl;
+				throw;
+			}
 		}
 		catch(...) {
 			cout << "something gone wrong in reading data" << endl;
@@ -57,8 +99,8 @@ class cSolve{
 		for (int i = 0; i < RUNS_AMOUNT; i++) {
 			results.clear();
 			const time_point<high_resolution_clock> start{std::chrono::high_resolution_clock::now()};
-			for (const string& line : lines) {
-				results.push_back(solveLine(line));
+			for (int i = 0; i < lines.size(); i++) {
+				results.push_back(solveLine(i));
 			}
 			total_result = accumulate(results.begin(), results.end(), 0);
 			const time_point<high_resolution_clock> end{std::chrono::high_resolution_clock::now()};
@@ -80,6 +122,7 @@ class cSolve{
 int main(void) {
 	cSolve example("example.input");
 	cSolve cond("cond.input"); 
+	// 518930 - too low
 	example.solve();
 	cond.solve();
 	cout << endl;
